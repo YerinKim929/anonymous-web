12b7703f62f7ba922dee6b5b8704d59b6204ec87
Yerin Kim <nirey929@gmail.com>
2022-08-03 16:27:11 +0900

    Add picture section

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
-// VISUAL애서 애플리케이션 제목이 나타나는 효과 (하나 더 추가할 것)
+// VISUAL애서 애플리케이션 제목이 나타나는 효과
 const fadeEls = document.querySelectorAll(".visual .fade-in");
 fadeEls.forEach(function (fadeEl, index) {
   gsap.to(fadeEl, 1, {
     delay: (index + 1) * 0.7, // 0.7 1.4
     opacity: 1,
   });
+});
+
+// new Swiper(선택자, 옵션)
+new Swiper(".picture .swiper-container", {
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000, // 5초
+  },
 });
